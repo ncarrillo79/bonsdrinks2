f1096ed8af9081a5d595e41f72f9f4184b4ea555
Norma <ncarrillo79@hotmail.com>
2021-02-28 00:24:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
+//import Home from "./components/Home/Home";
+import Contato from "./components/Contato/Contato";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Home />
+        {/*<Home />*/}
+        <Contato/>
         <Footer />
       </div>
     );
---
+++ src/components/Contato/Contato.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+//import './../../App.css'
+import './Contato.css'
+
+class Contato extends Component {
+  render() {
+    return (
+      <div className="Form">
+        <h1>Contato</h1>
+        <div className="MainForm">
+        <img className="img" src="https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg" alt="contato"></img>
+          <form className="Inputs">
+            <label htmlFor="name">Nome:</label>
+            <input name="name" type="text" />
+
+            <label htmlFor="email">E-mail:</label>
+            <input name="email" type="text" />
+
+            <label htmlFor="message">Mensagem:</label>
+            <textarea name="message" rows="10" />
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Contato;
